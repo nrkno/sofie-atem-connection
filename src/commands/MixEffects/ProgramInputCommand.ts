45301f22d4d8f3a7e66316420ca081a11a56c227
Balte de Wit <balte.dewit@gmail.com>
2018-04-17 16:41:31 +0200

    feat(ProgramInputCommand)

---
+++ src/commands/MixEffects/ProgramInputCommand.ts
@@ -0,0 +1,36 @@
+import IAbstractCommand from '../AbstractCommand'
+import { AtemState, MixEffect } from '../../lib/atemState'
+import { Util } from '../../lib/atemUtil'
+
+export class ProgramInputCommand implements IAbstractCommand {
+	resolve: () => void
+	reject: () => void
+
+	rawName = 'PrgI'
+	packetId: number
+
+	source: number
+	mixEffect: number
+
+	deserialize (rawCommand: Buffer) {
+		this.mixEffect = rawCommand[0]
+		this.source = Util.parseNumber(rawCommand.slice(2, 4))
+	}
+
+	serialize () {
+		let rawCommand = 'CPgI'
+		return new Buffer([...Buffer.from(rawCommand), this.mixEffect, 0x00, this.source >> 8, this.source & 0xFF])
+	}
+
+	getAttributes () {
+		return {
+			mixEffect: this.mixEffect,
+			source: this.source
+		}
+	}
+
+	applyToState (state: AtemState) {
+		let mixEffect = state.video.getMe(this.mixEffect)
+		mixEffect.programInput = this.source
+	}
+}
