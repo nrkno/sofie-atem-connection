e9787be38a88b862307a16bd780e1c79e98db791
Balte de Wit <balte.dewit@gmail.com>
2018-04-21 17:03:20 +0200

    feat(Preview Transitions)

--- src/atem.ts
+++ src/atem.ts
@@ -213,6 +213,13 @@ export class Atem extends EventEmitter {
 		return this.sendCommand(command)
 	}
 
+	previewTransition (on: boolean, me = 0) {
+		let command = new Commands.PreviewTransitionCommand()
+		command.preview = on
+		command.mixEffect = me
+		return this.sendCommand(command)
+	}
+
 	private _mutateState (command: AbstractCommand) {
 		command.applyToState(this.state)
 		this.emit('stateChanged', this.state, command)
---
+++ src/commands/MixEffects/Transition/TransitionPreviewCommand.ts
@@ -0,0 +1,36 @@
+import IAbstractCommand from '../../AbstractCommand'
+import { AtemState } from '../../../lib/atemState'
+
+export class PreviewTransitionCommand implements IAbstractCommand {
+	resolve: () => void
+	reject: () => void
+
+	rawName = 'TrPr' // this seems unnecessary.
+	packetId: number
+
+	mixEffect: number
+	preview: boolean
+
+	deserialize (rawCommand: Buffer) {
+		this.mixEffect = rawCommand[0]
+		this.preview = rawCommand[1] === 1
+	}
+
+	serialize () {
+		console.log(this.mixEffect)
+		let rawCommand = 'CTPr'
+		return new Buffer([...Buffer.from(rawCommand), this.mixEffect, this.preview, 0x00, 0x00])
+	}
+
+	getAttributes () {
+		return {
+			mixEffect: this.mixEffect,
+			preview: this.preview
+		}
+	}
+
+	applyToState (state: AtemState) {
+		let mixEffect = state.video.getMe(this.mixEffect)
+		mixEffect.transitionPreview = this.preview
+	}
+}
