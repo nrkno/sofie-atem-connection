c991b0401585dbdbcb856c5c575ba7595f5d8ed7
Balte de Wit <balte.dewit@gmail.com>
2018-04-25 07:09:54 +0200

    chore: clean up

--- src/lib/atemUtil.ts
+++ src/lib/atemUtil.ts
@@ -1,40 +1,4 @@
 export namespace Util {
-	export function parseNumber (bytes: Buffer): number {
-		let num = 0
-		for (let i = 0; i < bytes.length; i++) {
-			let byte = bytes[i]
-			num += byte
-			if (i < bytes.length - 1) num = num << 8
-		}
-		return num
-	}
-
-	export function parseString (bytes: Buffer): string {
-		let str = ''
-		for (let char of bytes) {
-			if (char === 0) break
-			str += String.fromCharCode(char)
-		}
-		return str
-	}
-
-	export function bufferToArray (buffers: Buffer): Array<number> {
-		let arr = []
-		for (let i of buffers) {
-			arr.push(i)
-		}
-		return arr
-	}
-
-	export function bufferToBytes (from: number, numberOfBytes: number): Array<number> {
-		let bytes = []
-		for (let i = 0; i <= numberOfBytes; i++) {
-			let shift = numberOfBytes - i - 1
-			bytes.push((from >> (8 * shift) & 0xFF))
-		}
-		return bytes
-	}
-
 	export function stringToBytes (str: string): Array<number> {
 		let array = []
 		for (let val of Buffer.from(str).values()) {
