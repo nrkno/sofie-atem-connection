9be9e6188e5ad5b4737dc78f480d920844ff7ec6
Balte de Wit <balte.dewit@gmail.com>
2018-05-01 08:19:44 +0200

    feat: export commands and substates

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ export * from './atem'
 export * from './state'
 
 import * as Enums from './enums'
+import * as Commands from './commands'
 export {
-	Enums
+	Enums,
+	Commands
+}
+
+import * as VideoState from './state/video'
+import * as AudioState from './state/audio'
+import * as MediaState from './state/media'
+import * as InfoState from './state/info'
+import * as InputState from './state/input'
+export {
+	VideoState,
+	AudioState,
+	MediaState,
+	InfoState,
+	InputState
 }
