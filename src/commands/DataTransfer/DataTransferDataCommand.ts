df1610907848f70a1dfa369af9b6026dff0ed34c
Balte de Wit <balte.dewit@gmail.com>
2018-05-17 07:44:47 +0200

    fix(Data Command): size property

--- src/commands/DataTransfer/DataTransferDataCommand.ts
+++ src/commands/DataTransfer/DataTransferDataCommand.ts
@@ -5,13 +5,15 @@ export class DataTransferDataCommand extends AbstractCommand {
 
 	properties: {
 		transferId: number,
+		size: number,
 		body: Buffer
 	}
 
 	serialize () {
+		if (typeof this.properties.size === 'undefined') this.properties.size = this.properties.body.length
 		const buffer = Buffer.alloc(4)
 		buffer.writeUInt16BE(this.properties.transferId, 0)
-		buffer.writeUInt16BE(this.properties.body.length, 2)
+		buffer.writeUInt16BE(this.properties.size, 2)
 
 		return Buffer.concat([Buffer.from('FTDa', 'ascii'), buffer, this.properties.body])
 	}
@@ -19,6 +21,7 @@ export class DataTransferDataCommand extends AbstractCommand {
 	deserialize (rawCommand: Buffer) {
 		this.properties = {
 			transferId: rawCommand.readUInt16BE(0),
+			size: rawCommand.readUInt16BE(2),
 			body: rawCommand.slice(4, 4 + rawCommand.readUInt16BE(2))
 		}
 	}
