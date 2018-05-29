5050fe8a1a27b4f2b7707dde843c94756a7e26e9
Balte de Wit <balte.dewit@gmail.com>
2018-05-29 07:22:09 +0200

    feat(Media): Media Pool Description Commands

---
+++ src/commands/Media/MediaPoolClipDescription.ts
@@ -0,0 +1,31 @@
+import { AtemState } from '../../state'
+import { ClipBank } from '../../state/media'
+import AbstractCommand from '../AbstractCommand'
+import { Util } from '../..'
+
+export class MediaPoolClipDescriptionCommand extends AbstractCommand {
+	rawName = 'MPCS'
+
+	mediaPool: number
+	properties: ClipBank
+
+	deserialize (rawCommand: Buffer) {
+		this.mediaPool = rawCommand[0]
+		this.properties = {
+			isUsed: rawCommand[1] === 1,
+			name: Util.bufToNullTerminatedString(rawCommand, 2, 64),
+			frameCount: rawCommand.readUInt16BE(66),
+			frames: []
+		}
+	}
+
+	applyToState (state: AtemState) {
+		const newProps = { ...this.properties }
+
+		// @todo: check if the undefined check makes sense, maybe check for frameCount instead?
+		if (typeof state.media.clipPool[this.mediaPool - 1] !== 'undefined') {
+			newProps.frames = state.media.clipPool[this.mediaPool - 1].frames
+		}
+		state.media.clipPool[this.mediaPool - 1] = newProps
+	}
+}
