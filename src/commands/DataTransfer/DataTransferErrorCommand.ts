7e302e19554541b14e0f333685f71300e7df5bcf
Alex Van Camp <vancamp.alex@gmail.com>
2018-08-06 14:33:35 -0500

    feat(DataTransfer): add errorCode property to DataTransferErrorCommand

--- src/commands/DataTransfer/DataTransferErrorCommand.ts
+++ src/commands/DataTransfer/DataTransferErrorCommand.ts
@@ -4,12 +4,14 @@ export class DataTransferErrorCommand extends AbstractCommand {
 	rawName = 'FTDE'
 
 	properties: {
-		transferId: number
+		transferId: number,
+		errorCode: number
 	}
 
 	deserialize (rawCommand: Buffer) {
 		this.properties = {
-			transferId: rawCommand.readUInt16BE(0)
+			transferId: rawCommand.readUInt16BE(0),
+			errorCode: rawCommand.readUInt8(2)
 		}
 	}
 }
