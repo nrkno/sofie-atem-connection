f96d128fa7ebf00c609ae4c18b016af0d402f74c
Balte de Wit <balte.dewit@gmail.com>
2018-09-26 08:27:28 +0200

    chore: update build

--- dist/commands/DataTransfer/DataTransferFileDescriptionCommand.js
+++ dist/commands/DataTransfer/DataTransferFileDescriptionCommand.js
@@ -10,7 +10,7 @@ class DataTransferFileDescriptionCommand extends AbstractCommand_1.default {
         const buffer = Buffer.alloc(212);
         buffer.writeUInt16BE(this.properties.transferId, 0);
         if (this.properties.name)
-            buffer.write(this.properties.name, 2, 20);
+            buffer.write(this.properties.name, 2, 64);
         if (this.properties.description)
             buffer.write(this.properties.description, 66, 128);
         buffer.write(this.properties.fileHash, 194, 16);
