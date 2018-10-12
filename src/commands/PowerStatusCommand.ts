469ecc7e1fd76f5aa29758da43663227eb6c961a
Balte de Wit <balte.dewit@gmail.com>
2018-10-12 07:43:12 +0200

    chore: documentation

--- src/commands/PowerStatusCommand.ts
+++ src/commands/PowerStatusCommand.ts
@@ -1,6 +1,11 @@
 import AbstractCommand from './AbstractCommand'
 import { AtemState } from '../state'
 
+/**
+ * This command gets the power status from the Atem. As defined in
+ * DeviceProfile/productIdentifierCommand.ts the 2ME, 2ME 4K and the
+ * Broadcast Studio have 2 power supplies. All other models have 1.
+ */
 export class PowerStatusCommand extends AbstractCommand {
 	rawName = 'Powr'
 
