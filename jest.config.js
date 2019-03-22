98d16a8bd52de7bd68430b34a627fdb10b936819
Julian Waller <git@julusian.co.uk>
2019-03-22 12:56:51 +0000

    fix: upgrade dependencies to pass audit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globals: {
 		'ts-jest': {
-			tsConfigFile: 'tsconfig.jest.json'
+			tsConfig: 'tsconfig.jest.json'
 		}
 	},
 	moduleFileExtensions: [
@@ -9,7 +9,7 @@ module.exports = {
 		'js'
 	],
 	transform: {
-		'^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js'
+		'^.+\\.(ts|tsx)$': 'ts-jest'
 	},
 	testMatch: [
 		'**/__tests__/**/*.spec.(ts|js)'
