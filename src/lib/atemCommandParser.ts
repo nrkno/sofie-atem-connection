1e26d6d44d1363d3419459aa11f7e5129c596c06
Julian Waller <git@julusian.co.uk>
2019-06-20 21:23:38 +0100

    fix: improve commandParser typings

--- src/lib/atemCommandParser.ts
+++ src/lib/atemCommandParser.ts
@@ -1,14 +1,16 @@
 import * as Commands from '../commands'
 import AbstractCommand from '../commands/AbstractCommand'
 
+type CommandConstructor = new () => AbstractCommand
 export class CommandParser {
-	commands: {[key: string]: AbstractCommand} = {}
+	commands: {[key: string]: CommandConstructor} = {}
 
 	constructor () {
 		for (const cmd in Commands) {
 			try {
-				const rawName = new (Commands as any)[cmd]().rawName
-				this.commands[rawName] = (Commands as any)[cmd]
+				const cmdConstructor = (Commands as any)[cmd] as CommandConstructor
+				const rawName = new cmdConstructor().rawName
+				this.commands[rawName] = cmdConstructor
 			} catch (e) {
 				// wwwwhatever
 			}
@@ -18,8 +20,7 @@ export class CommandParser {
 	commandFromRawName (name: string): AbstractCommand | undefined {
 		if (this.commands[name]) {
 			// we instantiate a class based on the raw command name
-			return new (this.commands[name] as any)()
-			// return Object.create((this.commands as any)[name]['prototype'])
+			return new this.commands[name]()
 		}
 		return undefined
 	}
