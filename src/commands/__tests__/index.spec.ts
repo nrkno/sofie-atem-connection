8bd900926cea719aa9f6cff3c43207ef1df05aaa
Julian Waller <git@julusian.co.uk>
2019-06-20 10:07:36 +0100

    fix: More command test data mangling

--- src/commands/__tests__/index.spec.ts
+++ src/commands/__tests__/index.spec.ts
@@ -10,18 +10,90 @@ export interface TestCase {
 
 const idAliases: { [key: string]: string } = {
 	'auxBus': 'id',
-	'index': 'index'
+	'index': 'index',
+	'mixEffect': 'index',
+	// 'mixEffectIndex': 'index',
+	'multiViewerId': 'index',
+	'upstreamKeyerId': 'keyerIndex',
+	'mediaPlayerId': 'index',
+	'downstreamKeyerId': 'index',
+	'mediaPool': 'bank',
+	'frameIndex': 'index'
 }
-const propertyAliases: { [key: string]: string } = {
-	'programOutFollowFadeToBlack': 'followFadeToBlack'
+const propertyAliases: { [key: string]: string } = { // TODO - should these be done in the generator instead
+	'programOutFollowFadeToBlack': 'followFadeToBlack',
+	'mixEffectIndex': 'index',
+	'apiMajor': 'major',
+	'apiMinor': 'minor',
+	'dVE': 'DVEs',
+	'mixEffectBlocks': 'MEs',
+	'auxiliaries': 'auxilliaries',
+	'serialPort': 'serialPorts',
+	'hyperDecks': 'maxHyperdecks',
+	'videoSources': 'sources',
+	'previewTransition': 'preview',
+	'videoMode': 'mode',
+	'multiviewIndex': 'index',
+	'KeBP.keyerIndex': 'upstreamKeyerId',
+	'KeBP.mode': 'mixEffectKeyType',
+	'KePt.xPosition': 'positionX',
+	'KePt.yPosition': 'positionY',
+	'MRPr.index': 'macroIndex',
+	'MRcS.index': 'macroIndex',
+	'MPrp.index': 'macroIndex',
+	'filename': 'fileName',
+	'DskP.preMultipliedKey': 'preMultiply',
+	'inverse': 'invert',
+	'borderShadowEnabled': 'shadowEnabled'
 }
-const propertyConversion: { [key: string]: (v: any) => any } = {
+const propertyConversion: { [key: string]: (v: any) => any } = { // TODO - should these be done in the generator instead?
 	'balance': (v: number) => Math.round(v * 10) / 10,
-	'gain': (v: number) => Math.round(v * 100) / 100
+	'gain': (v: number) => Math.round(v * 100) / 100,
+	'TStP.gain': (v: number) => Math.round(v * 10),
+	'TStP.clip': (v: number) => Math.round(v * 10),
+	'borderSoftness': (v: number) => Math.round(v * 100),
+	'borderWidth': (v: number) => Math.round(v * 100),
+	'symmetry': (v: number) => Math.round(v * 100),
+	'xPosition': (v: number) => Math.round(v * 10000),
+	'yPosition': (v: number) => Math.round(v * 10000),
+	'TDvP.gain': (v: number) => Math.round(v * 10),
+	'TDvP.clip': (v: number) => Math.round(v * 10),
+	'KeCk.gain': (v: number) => Math.round(v * 10),
+	'hue': (v: number) => Math.round(v * 10),
+	'lift': (v: number) => Math.round(v * 10),
+	'ySuppress': (v: number) => Math.round(v * 10),
+	'maskLeft': (v: number) => Math.round(v * 1000),
+	'maskRight': (v: number) => Math.round(v * 1000),
+	'maskTop': (v: number) => Math.round(v * 1000),
+	'maskBottom': (v: number) => Math.round(v * 1000),
+	'KeLm.clip': (v: number) => Math.round(v * 10),
+	'KeLm.gain': (v: number) => Math.round(v * 10),
+	'handlePosition': (v: number) => Math.round(v * 10000),
+	'hash': (v: string) => Buffer.from(v, 'base64').toString('ascii'),
+	'DskP.clip': (v: number) => Math.round(v * 10),
+	'DskP.gain': (v: number) => Math.round(v * 10),
+	'size': (v: number) => Math.round(v * 100),
+	'softness': (v: number) => Math.round(v * 100),
+	'borderHue': (v: number) => Math.round(v * 10),
+	'borderInnerWidth': (v: number) => Math.round(v * 100),
+	'borderLuma': (v: number) => Math.round(v * 10),
+	'borderOuterWidth': (v: number) => Math.round(v * 100),
+	'borderSaturation': (v: number) => Math.round(v * 10),
+	'lightSourceDirection': (v: number) => Math.round(v * 10),
+	'KeDV.positionX': (v: number) => Math.round(v * 1000),
+	'KeDV.positionY': (v: number) => Math.round(v * 1000),
+	'KeDV.sizeX': (v: number) => Math.round(v * 1000),
+	'KeDV.sizeY': (v: number) => Math.round(v * 1000),
+	'KeDV.rotation': (v: number) => Math.round(v * 10),
 }
 
 describe('Auto v8', () => {
 	const commandParser = new CommandParser()
+
+	// TODO - this wants to work on v7.2 for now, with a small set of data for v8
+
+	// TODO - track which commands havent had a serialize/deserialize called and cause a failure on that, or is lack of test percentage good enough?
+	// TODO - some commands appear to not have very random data. Will some not work because of their c# implementation?
 
 	for (let i = 0; i < DataV8.length; i++) {
 		const testCase = DataV8[i]
@@ -44,6 +116,7 @@ describe('Auto v8', () => {
 				for (const key in cmd) {
 					const newName = idAliases[key]
 					if (cmd.hasOwnProperty(key) && newName) {
+						if (!cmd.properties) cmd.properties = {}
 						cmd.properties[newName] = (cmd as any)[key]
 					}
 				}
@@ -51,20 +124,26 @@ describe('Auto v8', () => {
 				const lowerCommand: { [key: string]: any } = {}
 				for (const key in testCase.command) {
 					const newKey = key[0].toLowerCase() + key.substring(1)
-					const newKey2 = propertyAliases[newKey] || newKey
+					const newKey2 = propertyAliases[`${name}.${newKey}`] || propertyAliases[newKey] || newKey
 
-					const converter = propertyConversion[newKey]
+					const converter = propertyConversion[`${name}.${newKey}`] || propertyConversion[newKey]
 					const newValue = converter ? converter(testCase.command[key]) : testCase.command[key]
 					lowerCommand[newKey2] = newValue
 				}
 
+				delete lowerCommand['test1']
+				delete lowerCommand['test2']
+				delete lowerCommand['test3']
+				delete lowerCommand['unknown']
+
 				expect(cmd.properties).toEqual(lowerCommand)
 			} else {
 				// TODO command might be a setter and so serializable
-				console.log('command is not deserializable')
+				// console.log('command is not deserializable')
 
 				// Otherwise ignore, as its not supported
-				// expect(false).toBeTruthy()
+				// TODO - should they be ignored in here, or filtered in the generator project?
+				expect(false).toBeTruthy()
 			}
 		})
 	}
