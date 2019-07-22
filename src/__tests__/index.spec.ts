7208ee44d020b77fff2870c609bf41462597940a
Julian Waller <git@julusian.co.uk>
2019-07-22 17:14:48 +0100

    feat: Add test for api methods which change command based on version

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { Atem } from '../index'
+import { Atem, Enums } from '../index'
 
 function cleanupAtem (atem: Atem) {
 	const atem2 = atem as any
@@ -17,5 +17,108 @@ test('Simple test', async () => {
 		expect(nb).toBeTruthy()
 	} finally {
 		cleanupAtem(nb)
+	}
+})
+
+function createConnection (apiVersion: Enums.ProtocolVersion) {
+	const conn = new Atem({ debug: true })
+	// conn.on('error', () => null)
+	conn.sendCommand = jest.fn()
+	conn.state.info.apiVersion = apiVersion
+
+	return conn
+}
+
+test('setSuperSourceProperties - 7.2', async () => {
+	const conn = createConnection(Enums.ProtocolVersion.V7_2)
+	try {
+		expect(conn.sendCommand).not.toHaveBeenCalled()
+
+		await conn.setSuperSourceProperties(2, {
+			artPreMultiplied: true,
+			artOption: Enums.SuperSourceArtOption.Background
+		})
+		expect(conn.sendCommand).toHaveBeenCalledTimes(1)
+		expect(conn.sendCommand).toHaveBeenNthCalledWith(1, {
+			rawName: 'CSSc',
+			flag: 12,
+			properties: {
+				artOption: 0,
+				artPreMultiplied: true
+			}
+		})
+	} finally {
+		cleanupAtem(conn)
+	}
+})
+
+test('setSuperSourceProperties - 8.0', async () => {
+	const conn = createConnection(Enums.ProtocolVersion.V8_0)
+	try {
+		expect(conn.sendCommand).not.toHaveBeenCalled()
+
+		await conn.setSuperSourceProperties(2, {
+			artPreMultiplied: true,
+			artOption: Enums.SuperSourceArtOption.Background
+		})
+		expect(conn.sendCommand).toHaveBeenCalledTimes(1)
+		expect(conn.sendCommand).toHaveBeenNthCalledWith(1, {
+			rawName: 'CSSc',
+			ssrcId: 2,
+			flag: 12,
+			properties: {
+				artOption: 0,
+				artPreMultiplied: true
+			}
+		})
+	} finally {
+		cleanupAtem(conn)
+	}
+})
+
+test('setSuperSourceBorder - 7.2', async () => {
+	const conn = createConnection(Enums.ProtocolVersion.V7_2)
+	try {
+		expect(conn.sendCommand).not.toHaveBeenCalled()
+
+		await conn.setSuperSourceBorder(2, {
+			borderBevelSoftness: 12,
+			borderLuma: 3
+		})
+		expect(conn.sendCommand).toHaveBeenCalledTimes(1)
+		expect(conn.sendCommand).toHaveBeenNthCalledWith(1, {
+			rawName: 'CSSc',
+			flag: 139264,
+			properties: {
+				borderBevelSoftness: 12,
+				borderLuma: 3
+			}
+		})
+	} finally {
+		cleanupAtem(conn)
+	}
+})
+
+test('setSuperSourceBorder - 8.0', async () => {
+	const conn = createConnection(Enums.ProtocolVersion.V8_0)
+	try {
+		expect(conn.sendCommand).not.toHaveBeenCalled()
+
+		await conn.setSuperSourceBorder(2, {
+			borderBevelSoftness: 12,
+			borderLuma: 3
+		})
+		expect(conn.sendCommand).toHaveBeenCalledTimes(1)
+		expect(conn.sendCommand).toHaveBeenNthCalledWith(1, {
+			rawName: 'SSBd',
+			ssrcId: 2,
+			flag: 1088,
+			properties: {
+				borderBevelSoftness: 12,
+				borderLuma: 3
+			}
+		})
+	} finally {
+		cleanupAtem(conn)
 	}
 })
