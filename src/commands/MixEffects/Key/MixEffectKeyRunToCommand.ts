8a3e4b74be482b321d443893cc917fe92c8452d3
Julian Waller <git@julusian.co.uk>
2021-02-26 20:13:33 +0000

    fix: bulk of test fixing

--- src/commands/MixEffects/Key/MixEffectKeyRunToCommand.ts
+++ src/commands/MixEffects/Key/MixEffectKeyRunToCommand.ts
@@ -5,16 +5,12 @@ export class MixEffectKeyRunToCommand extends BasicWritableCommand<{ keyFrameId:
 
 	public readonly mixEffect: number
 	public readonly upstreamKeyerId: number
-	public readonly keyFrameId: number
-	public readonly direction: number
 
 	constructor(mixEffect: number, upstreamKeyerId: number, keyFrameId: number, direction: number) {
 		super({ keyFrameId, direction })
 
 		this.mixEffect = mixEffect
 		this.upstreamKeyerId = upstreamKeyerId
-		this.keyFrameId = keyFrameId
-		this.direction = direction
 	}
 
 	public serialize(): Buffer {
@@ -22,8 +18,8 @@ export class MixEffectKeyRunToCommand extends BasicWritableCommand<{ keyFrameId:
 		buffer.writeUInt8(2, 0)
 		buffer.writeUInt8(this.mixEffect, 1)
 		buffer.writeUInt8(this.upstreamKeyerId, 2)
-		buffer.writeUInt8(this.direction, 5)
-		buffer.writeUInt8(this.keyFrameId, 4)
+		buffer.writeUInt8(this.properties.direction, 5)
+		buffer.writeUInt8(this.properties.keyFrameId, 4)
 
 		return buffer
 	}
