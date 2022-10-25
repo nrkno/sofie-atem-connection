01efa6693f04678246784eb9af5deba0a720018a
Julian Waller <git@julusian.co.uk>
2022-10-25 16:18:13 +0100

    chore: update jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	globals: {
-		'ts-jest': {
-			tsconfig: 'tsconfig.json',
-		},
-	},
 	moduleFileExtensions: ['ts', 'js'],
 	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest',
+		'^.+\\.(ts|tsx)$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.json',
+			},
+		],
 	},
 	testMatch: ['**/__tests__/**/*.spec.(ts|js)'],
 	setupFilesAfterEnv: ['jest-extended/all'],
