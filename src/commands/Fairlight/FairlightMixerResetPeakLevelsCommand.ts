d670fb5e1b0b0b7030785c0fa4d78888d09750d5
Julian Waller <git@julusian.co.uk>
2022-11-20 23:27:59 +0000

    fix: FairlightMixerResetPeakLevelsCommand

--- src/commands/Fairlight/FairlightMixerResetPeakLevelsCommand.ts
+++ src/commands/Fairlight/FairlightMixerResetPeakLevelsCommand.ts
@@ -3,15 +3,23 @@ import { BasicWritableCommand } from '../CommandBase'
 export class FairlightMixerResetPeakLevelsCommand extends BasicWritableCommand<{ all: boolean; master: boolean }> {
 	public static readonly rawName = 'RFLP'
 
+	public tmp = 0
+
 	public serialize(): Buffer {
 		const buffer = Buffer.alloc(4)
 
 		let val = 0
 		if (this.properties.all) {
 			val |= 1 << 0
+
+			// some magic number that is needed for this to work
+			buffer.writeUInt8(0x01, 1)
 		}
 		if (this.properties.master) {
 			val |= 1 << 1
+
+			// some magic number that is needed for this to work
+			buffer.writeUInt8(0x04, 3)
 		}
 
 		buffer.writeUInt8(val, 0)
