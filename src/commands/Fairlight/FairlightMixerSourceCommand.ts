e7c8cc9fbe327ff1f3a95e750845a0211c852f67
Keith Senti <ksenti@gmail.com>
2023-07-07 00:54:24 -0500

    revert

--- src/commands/Fairlight/FairlightMixerSourceCommand.ts
+++ src/commands/Fairlight/FairlightMixerSourceCommand.ts
@@ -144,7 +144,6 @@ export class FairlightMixerSourceUpdateCommand extends DeserializedCommand<
 
 		input.sources[sourceIdStr] = {
 			...oldSource,
-			...Util.omit(oldSource?.levels),
 			equalizer: {
 				...oldSource?.equalizer,
 				enabled: this.properties.equalizerEnabled,
